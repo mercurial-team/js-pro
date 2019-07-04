fbc8255ba44b4502074803457e2bdabbb9e8a82e
mercurial-team <mercurial-team@yandex.ru>
2019-07-04 18:53:55 +0400

    Выполнение 1 урока ДЗ
    1) Написаны стили
    2) К карточке добавлено изображение
    3) Запятые появляются из за того, что массив преобразуется в строку.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Keyboard', price: 200},
-    {id: 3, title: 'Mouse', price: 47},
-    {id: 4, title: 'Gamepad', price: 87},
-    {id: 5, title: 'Chair', price: 187},
+    {id: 1, title: 'Notebook', price: 2000, img: "images/product.jpg"},
+    {id: 2, title: 'Keyboard', price: 200, img: "images/product.jpg"},
+    {id: 3, title: 'Mouse', price: 47, img: "images/product.jpg"},
+    {id: 4, title: 'Gamepad', price: 87, img: "images/product.jpg"},
+    {id: 5, title: 'Chair', price: 187, img: "images/product.jpg"},
+	{id: 6},
 ];
 
-const renderProduct = (title, price) => {
+const renderProduct = (title = 'Неизвестный продукт', price = 0, img = "images/product.jpg") => {
     return `<div class="product-item">
+				<img src = ${img} class = "product_image"></img>
                 <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="buy-btn">Купить</button>
+                <p>Цена: ${price}</p>
+                <button class="buy-btn btn">Купить</button>
             </div>`
 };
 
 const renderPage = list => {
     const productsList = list.map(el => renderProduct(el.title, el.price));
-    document.querySelector(`.products`).innerHTML = productsList;
+    document.querySelector(`.products`).innerHTML = productsList.join(" ");
+	//Массив преобразуется в строку и записывается вместе с запятыми.
 };
 
 renderPage(products);
