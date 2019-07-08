4dd75a6729e95847d5fc4a45182bebb6d2a17fd1
mercurial-team <mercurial-team@yandex.ru>
2019-07-08 22:36:57 +0400

    Домашняя работа №2 + Бургер

--- js/main.js
+++ js/main.js
@@ -1,25 +1,95 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000, img: "images/product.jpg"},
-    {id: 2, title: 'Keyboard', price: 200, img: "images/product.jpg"},
-    {id: 3, title: 'Mouse', price: 47, img: "images/product.jpg"},
-    {id: 4, title: 'Gamepad', price: 87, img: "images/product.jpg"},
-    {id: 5, title: 'Chair', price: 187, img: "images/product.jpg"},
-	{id: 6},
-];
+class Products {
+    constructor(container=`.products`){
+        this.container = container;
+        this.data = [];
+        this.allProduct = [];
+        this.init();
+    }
+    init(){
+        this._fetchProducts();
+        this._render();
+		this.calcTotalPrice();
+    }
+	calcTotalPrice(){
+		this.totalPrice = 0;
+		for (let el of this.data) {
+			this.totalPrice += el.price;
+		}
+		return this.totalPrice;
+	}
+    _fetchProducts(){
+        this.data = [
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Keyboard', price: 200},
+            {id: 3, title: 'Mouse', price: 47},
+            {id: 4, title: 'Gamepad', price: 87},
+            {id: 5, title: 'Chair', price: 187},
+        ];
+    }
+    _render(){
+        const block = document.querySelector(this.container);
+        for (let el of this.data) {
+            const product = new ProductItem(el);
+            this.allProduct.push(product);
+            block.insertAdjacentHTML('beforeend', product.render());
+        }
+    }
+}
 
-const renderProduct = (title = 'Неизвестный продукт', price = 0, img = "images/product.jpg") => {
-    return `<div class="product-item">
-				<img src = ${img} class = "product_image"></img>
-                <h3>${title}</h3>
-                <p>Цена: ${price}</p>
-                <button class="buy-btn btn">Купить</button>
-            </div>`
-};
+class ProductItem {
+    constructor(el, img='https://placehold.it/200x150'){
+        this.title = el.title;
+        this.id = el.id;
+        this.price = el.price;
+        this.img = img;
+    }
+    render() {
+            return `<div class="product-item">
+                 <img src="${this.img}" alt="${this.title} class = "product_image"">
+                 <div class="desc">
+                     <h3>${this.title}</h3>
+                     <p> Цена: ${this.price}</p>
+                     <button class="buy-btn btn">Купить</button>
+                 </div>
+             </div>`
+    }
+}
 
-const renderPage = list => {
-    const productsList = list.map(el => renderProduct(el.title, el.price));
-    document.querySelector(`.products`).innerHTML = productsList.join(" ");
-	//Массив преобразуется в строку и записывается вместе с запятыми.
-};
+class Cart {
+    constructor(container=`.cart`){
+		this.container = container;
+        this.data = [];
+        this.allProduct = [];
+    }
+	addProductToCart(){
+		// будет добавлять товар в корзину
+	}
+	removeProductFromCart(){
+		// будем удалять товар из корзины
+	}
+	setQuantity(){
+		//Будем устанавливать количество конкретного товара для операций + -
+		//Или поля ввода количества товара
+	}
+	_fetchCartProducts(){
+		//Получаем данные от сервера для формирования корзины
+        this.data = [
+            {id: 1, title: 'Notebook', price: 2000, quantity: 1},
+            {id: 4, title: 'Gamepad', price: 87, quantity: 3},
+        ];
+    }
+	_renderCart(){
+		//будем отрисовыввать корзину тут
+	}
+	_renderCartItem(){
+		//будем тут отрисовывать единицу товара в корзине.
+		//Этот метод можно также создать в классе ProductItem, вместо текущего класса.
+	}
+	_calcTotalPrice(){
+		//В конце корзины нам наверняка потребуется общая стоимость
+	}
+	
 
-renderPage(products);
+}
+const products = new Products();
+console.log(products.calcTotalPrice())
